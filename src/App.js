23d1cb448dc8171d73eafa659922b3b8b78685ef
Alison Lee <alison2686@gmail.com>
2023-03-02 15:26:59 -0800

    data persistence

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   // delete book by id, pass in id, const updatedBooks, pass in book, filter each book, if true keep book, if false delete book
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
@@ -27,7 +39,8 @@ function App() {
 
   return (
     <div className='app'>
-      <BookList books={books} onDelete={deleteBookById} />
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
