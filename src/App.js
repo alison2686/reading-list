d20c76ef3e1b1934f3a1b15ac9df2e55be2ec7e2
Alison Lee <alison2686@gmail.com>
2023-03-06 21:49:40 +0000

    fetch list of records

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('https://localhost:3001/books');
+
+    setBooks(response.data);
+  };
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
@@ -26,14 +33,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
